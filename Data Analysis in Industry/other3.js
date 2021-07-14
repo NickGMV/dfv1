77223a701d32b1fc9c8406b361292e176753e4d5
nickGMVP <78032114+nickGMVP@users.noreply.github.com>
2021-07-14 09:40:15 +0100

    Update other3.js

--- Data Analysis in Industry/other3.js
+++ Data Analysis in Industry/other3.js
@@ -1,3 +1,14 @@
+
+
+/*using this function to indicate a load of js file in to speaker notes*/
 function test() {
   window.alert('test success!');
 }
+
+/*use this function to scout how to edit asides once speaker notes are up*/
+function find_parent (element) {
+  window.alert(element.parentNode.id, element.parentNode.className);}
+
+/* potential start on editting content of speaker notes*/
+function change_content(element,content) {
+  document.getElementById(element.parentNode.id).innerHTML+=content;}
