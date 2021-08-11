d56449150a2e0cf537f42086751d73d2f092d141
nickGMVP <78032114+nickGMVP@users.noreply.github.com>
2021-08-11 16:10:15 +0100

    Update editor.js

--- editordependencies/editor.js
+++ editordependencies/editor.js
@@ -100,16 +100,15 @@
 // experimental use this function to determine if elements are on screen before running reminder function (might remove this)
 		function isInViewport(element) {
                          const rect = element.getBoundingClientRect();
-                           return (
-                            rect.top >= 0 &&
-                                 rect.left >= 0 &&
-                          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+                           return (rect.top >= 0 && rect.left >= 0 && 
+				   rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && 
+				   rect.right <= (window.innerWidth || document.documentElement.clientWidth)
                                                              );
-                                                             }
+                                                             };
 
 	// uses prior function to determine if allerts should go out	
-		function should_show(element, content){
+		function should_show(element, content)
+                         {
 			if(isInViewport(element)){
 			window.alert(content);
 			}
