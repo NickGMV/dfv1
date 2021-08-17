f1c880a1441ca2a84a42aae2f262227491850e78
nickGMVP <78032114+nickGMVP@users.noreply.github.com>
2021-08-17 14:10:22 +0100

    Update editorinternal.js
    cleaning up the code ready for last push to completion

--- editordependencies/editorinternal.js
+++ editordependencies/editorinternal.js
@@ -5,45 +5,34 @@
 	for(let i = 0; i<divs.length;i++){
 	divs[i].setAttribute("contenteditable", true);
 	}}();
-		
+
+// this listens for psoition updates from the tracker pixels embedded in the 
+//presentation and scrolls the window to view the corresponding section in the editor
 window.addEventListener('storage',function(){
   var position = localStorage.position;
- if(position){
-	 
-  console.log(position);
-	//restore_editable();
+ if(position){ 
+  console.log("current speaker note position is " + position);
   document.getElementById(position).scrollIntoView({block:'end' ,behavior: 'smooth'})}
  }
 	);
-//reset flag variable once window is closed
-//window.addEventListener('beforeunload', function (e) {
-  // the absence of a returnValue property on the event will guarantee the browser unload happens
-	//change the editor flag back to zero
- //editor_flag = 0;
- //window.opener.location.reload();
-//});
 
-
-
+// this function creates the reminders for when speaker notes are being used
 function add_reminder(el){
   var content = prompt('set you reminder, it will pop up in speaker notes when you reach the slide');
   if(content){
   var parentID = el.parentNode.id;
-//  el.parentNode.innerHTML += ' <img id = "' + parentID + 'reminder"  height ="10" width="10" src="https://nickgmvp.github.io/dfv1/Data%20Analysis%20in%20Industry/alarm.svg" class = "reminder" onload = "should_show(this,\'' + content +'\')";>';
  el.parentNode.innerHTML += ' <img title="' + content + '" id = "' + parentID + 'reminder"  height ="10" width="10" src="https://nickgmvp.github.io/dfv1/editordependencies/alarm.svg" class = "reminder" ondblclick="this.remove();" onload ="if(this.parentNode.classList.contains(\'value\')){window.alert(\''+ content + '\')};">';                                                
   }	
 };
-	
-
-		
+//This function will clear all the user editted sections, the user must save afterwards to make this permanent.
+// *****Consider adding instant save here.***
   function reset_notes(){
          var to_reset = document.getElementsByClassName('editable');
 	  for(let i = 0; i<to_reset.length;i++){
 	  to_reset[i].innerHTML = " ";
 	  }
-  
   }
-
+// one of two functions that strips out all ther editabilty ready for when notes are passed back to the presentation.
 function remove_edittable2(){
 	var divs = document.getElementsByClassName('editable');
 	console.log(divs.length);
@@ -51,7 +40,8 @@ function remove_edittable2(){
 	divs[i].setAttribute("contenteditable", false);
 }}
 
-
+// selects and destroys all the reminders in the editor window, must save afterwards to make changes permanent
+//*** consider adding instant save here *** 
   function reset_reminders(){
     
        var del = document.getElementsByClassName('reminder');
@@ -60,11 +50,8 @@ function remove_edittable2(){
             }
   };
   
-  function prep(){ 
-  //  var saveLocation = localStorage.getItem('saveLocation'); 
-  console.log("saving data");
- } ;
-
+// this function ensures no "add reminder buttons" are saved to the presentation 
+//notes as they only work in the editor window
   function strip_reminder_buttons(){
   var to_strip = document.getElementsByClassName('reminder_button');
   while(to_strip.length!=0){
@@ -72,20 +59,24 @@ function remove_edittable2(){
   }
   };
 
-
+// function ot save and close the editor window.
   function save_and_close() {
+	  //generates and formats a save id using path
     var saveLocation = window.opener.location.pathname + "save_data"; 
-	  saveLocation = JSON.stringify(saveLocation);
+    saveLocation = JSON.stringify(saveLocation);
     console.log("location data is being saved is ..." + saveLocation);
+    //reformat elements to be reintegrated with presentation
     strip_reminder_buttons();
     remove_edittable2();
+    // create array for elements to be stored in and fill it
     var to_save = []; 
     var new_notes = document.getElementsByClassName('notes');
     for(let i=0; i< new_notes.length ;i++){
       to_save[i]= new_notes[i].innerHTML 
     } 
-	  
+    //format savedata to it can be regathered at load by the presentation.  
     save_data = JSON.stringify(to_save); 
+    // put in to browser storage
     localStorage.setItem(saveLocation,save_data);
     console.log("the save data has ..." + to_save.length + "entries"); 
     var save = localStorage.getItem(saveLocation); 
@@ -94,6 +85,8 @@ function remove_edittable2(){
 	 
   };
 
+// This function completely wipes browser of ALL data
+//** add user verification here also colour the button red
   function nuke(){
     console.log('nuke imminent...'); 
     console.log('3');
