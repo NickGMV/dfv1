425c038ac5712cffeebd2d5ca8d1a7a5f8ecb0ab
nickGMVP <78032114+nickGMVP@users.noreply.github.com>
2021-08-17 16:42:15 +0100

    Update editor.js

--- editordependencies/editor.js
+++ editordependencies/editor.js
@@ -145,7 +145,7 @@ function setup_edit_mode(){
 		
 		editorWindow.document.body.innerHTML += "<div id='user_controls'><button onclick = 'save_and_close();'> save updates</button><button onclick='reset_notes();'>reset notes</button><button onclick='reset_reminders();'>reset reminders</button><button id = 'clear_all' onclick = 'nuke();'> clear all memory </button></div>";
 		
-		editorWindow.document.head.innerHTML += "<link rel='stylesheet' type='text/css' href="+ newPathname +"'/editorstyles.css'>";
+		editorWindow.document.head.innerHTML += "<link rel='stylesheet' type='text/css' href='"+ newPathname +"/editorstyles.css'>";
 	        return true;
 		}
 
