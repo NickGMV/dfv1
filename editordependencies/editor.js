8fb2ddffb57a19d40c8aed3ddbea01d851003377
nickGMVP <78032114+nickGMVP@users.noreply.github.com>
2021-09-08 18:24:06 +0100

    Update editor.js

--- editordependencies/editor.js
+++ editordependencies/editor.js
@@ -6,7 +6,7 @@
 var pathName2 = window.location.href
 var pathArray = pathName2.split('/')
 var newPathname = "";
-for (i = 0; i < 2; i++) {
+for (i = 0; i < 4; i++) {
   newPathname += "/";
   newPathname += pathArray[i];
 	console.log(newPathname);
